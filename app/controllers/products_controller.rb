7d6bfd292080e7339edf2e94c805f9ff5656be34
austinb847 <austinb847@icloud.com>
2020-05-29 12:24:12 -0700

    add token_tag to get bootstrap form submitting properly

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,15 +6,26 @@ def index
 end
 
 def new
+  @product = Product.new
   render :new
+end
+
+def create
+  @product = Product.new(product_params)
+  if @product.save
+    redirect_to products_path
+  else
+    render :new
+  end
 end
 
 
 
 
-
-
-
+private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
 
 
 end
