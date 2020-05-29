4eb4be8221175310f366a5ac499684655bac62df
austinb847 <austinb847@icloud.com>
2020-05-29 16:56:33 -0700

    add all validations to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
   before_save(:capitalize_product)
+  validates :name, :country_of_origin,  presence: true
+  validates_numericality_of :cost, only_integer: true
+
 
   private
     def capitalize_product
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,10 @@ describe Product do
   it("titleizes the name of a product") do
     product = Product.create({name: "gold fish", cost: 1.00, country_of_origin: "USA"})
   end
+
+  it { should validate_presence_of :name }
+
+  it { should validate_numericality_of(:cost).only_integer }
+
+  it { should validate_presence_of :country_of_origin}
 end
