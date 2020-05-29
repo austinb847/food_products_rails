7a24e84fb1ed6803d889b6f11a9858e06a80a23a
austinb847 <austinb847@icloud.com>
2020-05-29 14:53:29 -0700

    add callback method to titleize names of products before saving to db

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  before_save(:capitalize_product)
+
+  private
+    def capitalize_product
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+
+  it("titleizes the name of a product") do
+    product = Product.create({name: "gold fish", cost: 1.00, country_of_origin: "USA"})
+  end
 end
