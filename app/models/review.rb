60fe9986d686628a082a985bc6de6dc557ce6e59
austinb847 <austinb847@icloud.com>
2020-05-29 17:13:13 -0700

    add all validations for reviews and products models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, :rating, :content_body, presence: true
+  # validates_numericality_of :rating, in: 1..5
+  validates_inclusion_of :rating, in: 1..5
+  validates_length_of :content_body, minimum: 50, maximum: 250
+  # validates_length_of :content_body, maximum: 250
 end
