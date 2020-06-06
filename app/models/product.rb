dd1f87bce6fde73f2dc469b6218ba7820500bb17
austinb847 <austinb847@icloud.com>
2020-06-06 12:04:41 -0700

    add scope for products that sortsproducts by most recenetly added

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   validates_numericality_of :cost, only_integer: true
 
   scope :search, -> (product_name) { where("LOWER(name) like ?", "%#{product_name}%")}
-
+  scopre :three_recently_added, -> { order(created_at: :desc).limit(3)}
   private
     def capitalize_product
       self.name = self.name.titleize
